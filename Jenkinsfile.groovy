815ffeaa732acc92ec19c86b9439aaab7d83ce8f
Zhao Zhang <zhao.zhang1@thoughtworks.com>
2022-08-04 00:59:29 +0800

    configure build step

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'Building..'
+                bat 'build.bat'
             }
         }
         stage('Test') {
